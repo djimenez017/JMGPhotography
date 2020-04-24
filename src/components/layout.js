f414812fd8dc12088b5648b5b5f8963a410d977c
Daniel Jimenez <djimenez017@gmail.com>
2020-04-23 22:30:51 -0700

    added required to the form

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
-
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import ReactGA from "react-ga"
